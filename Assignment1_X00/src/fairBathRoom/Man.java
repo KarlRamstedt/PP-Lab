2d618c75197a7ddeb4a821f11490dc6273258b2e
Björn Bengtsson <a16bjobe@hs.local>
2017-12-01 17:33:21 +0100

    Force push

--- Assignment1_X00/src/fairBathRoom/Man.java
+++ Assignment1_X00/src/fairBathRoom/Man.java
@@ -14,7 +14,6 @@ public class Man implements Runnable {
 		GlobalState.mutex.V();
 		
 		while (true) {
-<<<<<<< HEAD
 			GlobalState.mutex.P();
 			if(GlobalState.numberOfWomenInCS > 0){
 				GlobalState.numberOfDelayedMen++;
@@ -41,43 +40,6 @@ public class Man implements Runnable {
 				doThings();
 			} else {
 				GlobalState.mutex.V();
-=======
-			// fill functionality here
-			
-			if (GlobalState.menQ.peek() == this && MEN TURN){
-				GlobalState.mutex.P();
-			
-				if(GlobalState.numberOfWomenInCS > 0){
-					GlobalState.numberOfDelayedMen++;
-					if(!GlobalState.menQ.contains(this))
-						GlobalState.menQ.add(this);
-					GlobalState.mutex.V();
-					GlobalState.menSem.P();
-				}
-				
-				if(GlobalState.numberOfMenInCS < 4){ // lägg till om först i listan && om det är ditt köns tur
-					if (TooManyMen){
-						TooManyMen = false;
-						GlobalState.numberOfWaitingMen--;
-					}
-					GlobalState.numberOfMenInCS++;
-					GlobalState.signal();
-					
-					doThings();
-					System.out.println(getState());
-					GlobalState.mutex.P();
-					GlobalState.numberOfMenInCS--;
-					GlobalState.signal();	
-				
-					doThings();
-				} else {
-					if (TooManyMen == false){
-						GlobalState.numberOfWaitingMen++;
-						TooManyMen = true;
-					}
-					GlobalState.mutex.V();
-				}
->>>>>>> c5663d998bfdc582c94e520720f3f09d88c02ae8
 			}
 			System.out.println("M" + AndrewsProcess.currentAndrewsProcessId() + " is out.");
 		}
