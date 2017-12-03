578c4dca0359c551e4d929920b1ec586e0f169fa
Erik Rodriguez <b16eriro@hs.local>
2017-12-03 11:39:40 +0100

    Trying to solve using extra semaphores

--- Assignment1_X00/src/fairBathRoom/Man.java
+++ Assignment1_X00/src/fairBathRoom/Man.java
@@ -5,8 +5,6 @@ import se.his.iit.it325g.common.AndrewsProcess;
 
 public class Man implements Runnable {
 
-	boolean inQ = false;
-	
 	@Override
 	public void run() {
 		GlobalState.mutex.P();
@@ -15,37 +13,31 @@ public class Man implements Runnable {
 		
 		while (true){				//poop loop
 			GlobalState.mutex.P();
-			if ((GlobalState.wom > 0 || !GlobalState.menTurn) && !inQ){ 
+			if (GlobalState.wom > 0 || !GlobalState.menTurn){ 
 				GlobalState.delayedMen++;
 				GlobalState.mutex.V();
 				GlobalState.semMen.P();
 			}
 			GlobalState.increaseTurn();
 			if (GlobalState.men >= 4 || GlobalState.menQ.peek() != this){
-				if (inQ){
-					inQ = false;
-					GlobalState.quedMen--;
-				}
-				GlobalState.men++;
-				GlobalState.menQ.poll(); //remove from queue
-				GlobalState.signal(); //release to next man
-				
-				doThings(); //poop
-				System.out.println(getState());
-				
-				GlobalState.mutex.P();
-				GlobalState.men--;
-				GlobalState.menQ.add(this); //re-enter queue
-				GlobalState.signal();
-				
-				doThings(); //eat a burger to make more poop
-			} else {
-				if (!inQ){
-					GlobalState.quedMen++;
-					inQ = true;
-				}
-				GlobalState.mutex.V();
+				GlobalState.quedMen++;
+				GlobalState.semMen.V();
+				GlobalState.queSemMen.P();
+				GlobalState.quedMen--;
 			}
+			GlobalState.men++;
+			GlobalState.menQ.poll(); //remove from queue
+			GlobalState.signal(); //release to next man
+			
+			doThings(); //poop
+			System.out.println(getState());
+			
+			GlobalState.mutex.P();
+			GlobalState.men--;
+			GlobalState.menQ.add(this); //re-enter queue
+			GlobalState.signal();
+			
+			doThings(); //eat a burger to make more poop
 		}
 	}
 
@@ -54,7 +46,9 @@ public class Man implements Runnable {
 				+ "  in CS, state: nm:" + GlobalState.men +
 				" nw:" + GlobalState.wom +
 				" dm:" + GlobalState.delayedMen +
-				" dw:" + GlobalState.delayedWom;
+				" dw:" + GlobalState.delayedWom +
+				" qm:" + GlobalState.quedMen +
+				" qw:" + GlobalState.quedWom;
 	}
 	private void doThings(){ //represents that processes are staying in a state for a while
 		AndrewsProcess.uninterruptibleMinimumDelay(ThreadLocalRandom.current().nextInt(100, 500));
